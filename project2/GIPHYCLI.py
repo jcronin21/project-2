8d7e2e5111bc40e25e1739b68bcdf80d62fa2c6c
Jazz <jcronin@unca.edu>
2024-03-26 12:19:37 -0400

    I think I fixed the error

---
+++ project2/GIPHYCLI.py
@@ -0,0 +1,20 @@
+import click
+
+
+@click.group()
+def gif():
+    print("hello from giphy cli!")
+
+
+@gif.command()
+def trending():
+    print("trending subcommand called!")
+
+
+@gif.command()
+def search():
+    print("search subcommand called!")
+
+
+if __name__ == "__main__":
+    gif()
